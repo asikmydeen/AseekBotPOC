1fff2f0fa16be84027b5a520a0b668623717ff45
Asik Mydeen <ammydeen@amazon.com>
2025-03-26 22:56:23 -0700

    CORS fixed finally

--- deploy-config.sh
+++ deploy-config.sh
@@ -1,8 +1,4 @@
 export AWS_S3_BUCKET_NAME="aseek-bot-uploads"
 export FRONTEND_S3_BUCKET="aseekbot-frontend-useast1"
 export CLOUDFRONT_DISTRIBUTION_ID="E11ZQ2JYIAINQV"
-export API_URL="https://api-dev-ammydeen.alpha.aseekbot.ammydeen.people.aws.dev"
-
-# Step Functions settings
-export DOCUMENT_ANALYSIS_STATE_MACHINE_ARN="arn:aws:states:us-east-1:361769603480:stateMachine:DocumentAnalysisWorkflow"
-export DOCUMENT_ANALYSIS_STATUS_TABLE="DocumentAnalysisStatus"
+export API_URL="https://api-ammydeen.alpha.aseekbot.ammydeen.people.aws.dev"
