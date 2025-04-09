8bc21b1bc0aec3f6f8e9508357b90d9439c0a025
Asik Mydeen <ammydeen@amazon.com>
2025-04-08 19:11:50 -0700

    refactor: Remove compatibility AppSidebar component This commit removes the temporary AppSidebar compatibility file and updates the import in page.tsx to directly import from the sidebar directory.

--- app/components/AppSidebar.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-// This is a temporary compatibility file that will be removed once all imports are updated
-// It simply re-exports the AppSidebar component from its new location
-
-import AppSidebar from './sidebar/AppSidebar';
-export type { UploadedFile, AppSidebarProps } from './sidebar/AppSidebar';
-
-export default AppSidebar;
